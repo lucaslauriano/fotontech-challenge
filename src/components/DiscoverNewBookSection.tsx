6c92e4a49c332f11196922b62a41348dee21fe78
lucaslauriano <lucaslauriano@upnid.com>
2021-07-13 14:02:21 -0300

    Add new images to DiscoverNewBooks

--- src/components/DiscoverNewBookSection.tsx
+++ src/components/DiscoverNewBookSection.tsx
@@ -1,79 +1,87 @@
-import * as React from "react";
 import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import { Flex, Box, Stack, Text, Image as ChakraImage } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { Flex, Box, Stack } from "@chakra-ui/react";
 import HeadingBox from "./HeadingBox";
-import NewBookHeadLine from "./NewbookHeadLine";
+import NewBookBanner from "./NewbookBanner";
+import {
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+  CarouselProvider,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import { NONAME } from "dns";
+import { inherits } from "util";
 
-const variants = {
-  enter: (direction: number) => {
-    return {
-      x: direction > 0 ? 1000 : -1000,
-      opacity: 0,
-    };
-  },
-  center: {
-    zIndex: 1,
-    x: 0,
-    opacity: 1,
-  },
-  exit: (direction: number) => {
-    return {
-      zIndex: 0,
-      x: direction < 0 ? 1000 : -1000,
-      opacity: 0,
-    };
-  },
+const card = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
 };
 
-const swipeConfidenceThreshold = 10000;
-const swipePower = (offset: number, velocity: number) => {
-  return Math.abs(offset) * velocity;
-};
-
-export const DiscoverNewBookSection = () => {
-  const [[page, direction], setPage] = useState([0, 0]);
-  const paginate = (newDirection: number) => {
-    setPage([page + newDirection, newDirection]);
-  };
-
+const DiscoverNewBookSection = () => {
+  const [isFocused, setIsFocused] = useState(true);
   return (
     <Box mt="30px">
-      <AnimatePresence initial={false} custom={direction}>
-        <HeadingBox title="Discover new book" actionTitle="More" />
-        <motion.div
-          key={page}
-          drag="x"
-          exit="exit"
-          custom={direction}
-          initial="enter"
-          animate="center"
-          variants={variants}
-          transition={{
-            x: { type: "spring", stiffness: 300, damping: 30 },
-            opacity: { duration: 0.2 },
-          }}
-          dragConstraints={{ left: 0, right: 0 }}
-          dragElastic={1}
-          onDragEnd={(e, { offset, velocity }) => {
-            const swipe = swipePower(offset.x, velocity.x);
+      <HeadingBox title="Discover new book" actionTitle="More" />
 
-            if (swipe < -swipeConfidenceThreshold) {
-              paginate(1);
-            } else if (swipe > swipeConfidenceThreshold) {
-              paginate(-1);
-            }
-          }}
-        >
-          <Box maxWidth={1024} overflow="hidden">
-            <Flex mt="15px">
-              <Stack direction="row">
-                <NewBookHeadLine />
-              </Stack>
-            </Flex>
-          </Box>
-        </motion.div>
-      </AnimatePresence>
+      <CarouselProvider
+        naturalSlideWidth={200}
+        naturalSlideHeight={200}
+        isIntrinsicHeight={true}
+        totalSlides={3}
+      >
+        <Slider moveThreshold={1} style={{ top: "15px" }}>
+          <Slide
+            index={0}
+            style={{
+              width: "272px",
+              height: "139px",
+              bottom: "14px",
+              position: "relative",
+              marginLeft: isFocused ? "0px" : "10px",
+            }}
+            onFocus={() => {
+              setIsFocused((prevState) => !prevState);
+            }}
+          >
+            <NewBookBanner isFocused={isFocused} />
+          </Slide>
+          <Slide
+            index={0}
+            style={{
+              width: "250px",
+              height: "128px",
+              bottom: "14px",
+              position: "relative",
+              marginLeft: "10px",
+            }}
+            onFocus={() => {
+              setIsFocused(true);
+            }}
+          >
+            <NewBookBanner isFocused={isFocused} />
+          </Slide>
+          <Slide
+            index={0}
+            style={{
+              width: "250px",
+              height: "128px",
+              bottom: "14px",
+              position: "relative",
+              marginLeft: "10px",
+            }}
+            onFocus={() => {
+              setIsFocused(true);
+            }}
+          >
+            <NewBookBanner isFocused={isFocused} />
+          </Slide>
+        </Slider>
+      </CarouselProvider>
     </Box>
   );
 };
+
+export default DiscoverNewBookSection;
