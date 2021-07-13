4535301b758a23ad37c0b37aed8c7de0ddecc56c
lucaslauriano <lucaslauriano@upnid.com>
2021-07-13 14:18:43 -0300

    Creating VideoReviewSection component

---
+++ src/components/VideoReviewsSection.tsx
@@ -0,0 +1,20 @@
+import { Flex, Box, Image } from "@chakra-ui/react";
+import HeadingBox from "./HeadingBox";
+
+const VideoReviewsSection = () => {
+  return (
+    <Box mt="25px">
+      <HeadingBox title="Reviews of The Days" actionTitle=" Al Video" />
+      <Flex mt="10px">
+        <Image
+          src="/images/thumb.png"
+          alt="Video Thumb"
+          width="335px"
+          height="181px"
+        />
+      </Flex>
+    </Box>
+  );
+};
+
+export default VideoReviewsSection;
