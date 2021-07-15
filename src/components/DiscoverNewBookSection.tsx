02de30d7efe3f923c12d09caa6dd466be9236db9
lucaslauriano <lucaslauriano@upnid.com>
2021-07-15 13:38:49 -0300

    Update Carrousel at ./DiscoverNewBookSection

--- src/components/DiscoverNewBookSection.tsx
+++ src/components/DiscoverNewBookSection.tsx
@@ -1,20 +1,51 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-import { Box, Flex, Stack } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import HeadingBox from "./HeadingBox";
 import NewBookBanner from "./NewbookBanner";
-import TouchCarousel from "react-touch-carousel";
+import Carousel from "react-multi-carousel";
 
 const DiscoverNewBookSection = () => {
   const [isFocused, setIsFocused] = useState(true);
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 376, min: 272 },
+      items: 1,
+      partialVisibilityGutter: 40,
+    },
+  };
+
   return (
     <Box mt="30px">
       <HeadingBox title="Discover new book" actionTitle="More" />
 
-      <Box width="100%" maxWidth={1024} overflow="hidden">
-        <Flex mt="15px">
-          <NewBookBanner />
-        </Flex>
+      <Box width="100%" maxWidth={1024}>
+        <Carousel
+          responsive={responsive}
+          ssr={true}
+          sliderClass={"item"}
+          removeArrowOnDeviceType={["tablet", "mobile"]}
+          focusOnSelect={true}
+          additionalTransfrom={0}
+          partialVisible
+        >
+          {["1", 2, 3].map((item, index) => (
+            <NewBookBanner key={index} />
+          ))}
+        </Carousel>
       </Box>
     </Box>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import Fonts from "../styles/Fonts";
+import "react-multi-carousel/lib/styles.css";
+import "../styles/styles.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
